15c1688f5b8787d7c4219387040c60592d45948f
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-25 16:50:21 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int e;
+	int f;
+
+	for (e = 1; f <= 10; f++)
+	{
+		for (e = 97; e <= 122; e++)
+		{
+			_putchar(e);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
+
