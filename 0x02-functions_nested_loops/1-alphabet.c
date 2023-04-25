48ebcba2375db736f0aeeb4baba5ad0771cbf900
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-25 16:08:29 -0700

    pseudo putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Description - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int a=97;
+	while (a <= 122);
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+	return (0);
+}
