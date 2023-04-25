5ac7331db0d1a09e66b1bd07e8a50248651f8b10
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-24 17:51:48 -0700

    more alphabets

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Prints alphabets in lowercase except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		if (a == 101 || a == 113)
+		{
+			a++;
+			continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
