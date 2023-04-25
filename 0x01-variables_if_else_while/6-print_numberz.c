f4776f75ada33524f9b99c8c0b21f5ac6d1f95d7
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-24 18:23:53 -0700

    more numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Prints numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
