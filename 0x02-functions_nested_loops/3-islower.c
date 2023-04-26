09ed72c13077d9cea9bede39cfcb2ee699c43711
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-25 18:26:06 -0700

    check for lowercase alphabet

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - Checks for lowercase alphabets.
+ * Return: 1 for lowercase
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
+
