0681f71839b55b3803d960173459b458bb6d711f
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-26 13:09:26 -0700

    count to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_to_98 - prints 0 to 98
+ *
+ *@n: number to start printing from
+ *
+ * Return: sum
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+		{
+		_putchar(n);
+		_putchar(44);
+		_putchar(32);
+		if (i > 98)
+			{
+			break;
+			}
+		}
+}
