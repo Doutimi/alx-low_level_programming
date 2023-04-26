3424f56474882a13cb105becba3f0b5807d49d2f
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-26 10:13:52 -0700

    adding integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - adds integers
+ *
+ *@a: integer to be added
+ *@b: integer to be added
+ *
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
