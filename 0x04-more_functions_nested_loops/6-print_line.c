87a86e9c19811679abde067a9034d700bcb15d9d
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-28 16:34:33 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_line - print a line
+*@n: number of times the line should be printed
+*
+* Return: no return
+*/
+
+void print_line(int n);
+{
+	int n;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar(95);
+		}
+	_putchar('\n');
+	}
+}
