c91771f15254377007b5cb2ee5b79d6d2fd73f63
Doutimi <isenahdoutimiebi@gmail.com>
2023-04-28 00:57:30 -0700

    multiply integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*mul - multiplies 2 digits
+*@a: integer to be multiplied
+*@b: integer to be multiplied
+* Return: 1 if uppercase, otherwise 0
+*/
+
+int mul(int a, int b)
+{
+	int i;
+
+	for (i = a * b)
+	{
+		return (i);
+	}
+	_putchar('\n');
+}
