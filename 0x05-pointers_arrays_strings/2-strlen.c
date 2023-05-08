fe54eab1d787dab38f4f4545688f9d09ff6f8e57
Doutimi <isenahdoutimiebi@gmail.com>
2023-05-08 07:21:15 -0700

    count strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: string to be counted
+ * Return: length
+ */
+
+int _strlen(char *s);
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
