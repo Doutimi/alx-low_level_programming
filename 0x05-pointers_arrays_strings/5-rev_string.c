714bfa7b1e876c13112cf1430449a3452aa2fc3f
Doutimi <isenahdoutimiebi@gmail.com>
2023-05-08 08:33:31 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: string 
+ * return: void
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int index = 0;
+	int i;
+
+	while (s[index] != '\0')
+	counter++;
+	for (i = 0; i < index; i++)
+	{
+		index--;
+		rev = s[i];
+		s[i] = s[index];
+		s[index] = rev;
+	}
+}
