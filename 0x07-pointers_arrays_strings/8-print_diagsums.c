59ec4f975fb965d722a42724364bc41b2db07af9
Doutimi <isenahdoutimiebi@gmail.com>
2023-05-14 09:59:16 -0700

    diagsum

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+	int j, k;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (j = 0; j <= (size * size); j = j + size + 1)
+	{
+		sum1 = sum1 + a[j];
+	}
+
+	for (k = size - 1; k <= (size * size); k = k + size - 1)
+	{
+		sum2 = sum2 + a[k];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
