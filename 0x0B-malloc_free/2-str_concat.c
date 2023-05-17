ec015df6eb6f1ff63792ad12fadc254e58f75093
Doutimi <isenahdoutimiebi@gmail.com>
2023-05-16 18:50:28 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,32 +9,35 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, d = 0;
-	char *s;
+	char *conct;
+	int a, b;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
+
 	if (s2 == NULL)
-		s2 = " ";
-	while (s1[a])
+		s2 = "";
+
+		a = b = 0;
+	while (s1[a] != '\0')
 		a++;
-	while (s2[b])
-	{
+
+	while (s2[b] != '\0')
 		b++;
-	}
-	d = a + b;
-	s = malloc((sizeof(char) * d) + 1);
-	if (s == NULL)
+	conct = malloc(sizeof(char) * (a + b + 1));
+		if (conct == NULL)
 		return (NULL);
-	while (c < d)
+	a = b = 0;
+	while (s1[a] != '\0')
 	{
-		if (c <= a)
-			s[c] = s1[c];
-		if (c >= a)
-			s[c] = s2[b];
-			b++;
-		c++;
+		conct[a] = s1[a];
+		a++;
 	}
-	s[k] = '\0';
-	return (s);
+	while (s2[b] != '\0')
+	{
+		conct[a] = s2[b];
+		a++, bi++;
+	}
+	conct[a] = '\0';
+	return (conct);
 }
