7e207fe506f642fffc772b2c83c023f391fa6a50
Doutimi <isenahdoutimiebi@gmail.com>
2023-05-19 15:13:11 -0700

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,9 +2,10 @@
 
 /**
 *main - entry point
-*@argc -The number of command line arguments
-*@argv -An array containing the program command line arguments
-*Return: always 0
+*@argc: The number of command line arguments
+*@argv: An array containing the program command line arguments
+*
+* Return: always 0
 */
 int main(int argc, char *argv[])
 {
