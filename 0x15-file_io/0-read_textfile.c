606b538b10b525462e88ce669544e1412403ffb6
Doutimi <isenahdoutimiebi@gmail.com>
2023-06-13 16:54:32 -0700

    read a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,10 +20,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
-	r = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, r);
+	r = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	free(buf);
+	free(buffer);
 	close(fd);
 	return (w);
 }
