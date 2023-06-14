e195f6a023bba54f7116ab4fe0ec782e0d5b752f
Doutimi <isenahdoutimiebi@gmail.com>
2023-06-13 17:52:37 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,13 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i];)
 			i++;
 	}
 
+	fd = open(filename, O_WRONLY | O_APPEND);
 	w = write(fd, text_content, i);
 
 	if (fd == -1 || w == -1)
