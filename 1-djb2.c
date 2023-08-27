7e60f1849048f587867725f63b4ebef1248f8338
Doutimi <isenahdoutimiebi@gmail.com>
2023-08-27 05:58:32 +0100

    task 1

--- 1-djb2.c
+++ 1-djb2.c
@@ -9,12 +9,12 @@
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
-	int a;
+	int c;
 
 	hash = 5381;
-	while ((a = *str++))
+	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + a; /* hash * 33 + a */
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
 	return (hash);
 }
